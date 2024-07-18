c4b055cc90c9d4dae84d571a1faed89b4c29fd32
shuong <shuongnguyen.work@gmail.com>
2024-07-18 15:06:01 +0700

    update signals

--- core_app/apps.py
+++ core_app/apps.py
@@ -4,3 +4,6 @@ from django.apps import AppConfig
 class CoreAppConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "core_app"
+
+    def ready(self):
+        import core_app.signals  # Import module signals
