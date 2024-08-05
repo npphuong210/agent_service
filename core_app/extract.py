b5ceed7c9e3a97c631f8f5884df3dbc50a6aed4f
neiths <thienkute01022002@gmail.com>
2024-08-05 16:19:10 +0700

    update

--- core_app/extract.py
+++ core_app/extract.py
@@ -9,32 +9,36 @@ def extract(ai_response: str, user_message: str) -> dict:
     summary = ""
     ai_hashtags = []
     combined_hashtags = []
+    cut_message = ""
 
-    # Extract summary from AI response
-    summary_pattern = re.compile(r'Summary:\s*(.*?)\s*Hashtag:', re.DOTALL)
-    summary_match = summary_pattern.search(ai_response)
-    if summary_match:
-        summary = summary_match.group(1).strip()
+    if ai_response:
+        # Extract summary from AI response
+        summary_pattern = re.compile(r'Summary:\s*(.*?)\s*Hashtag:', re.DOTALL)
+        summary_match = summary_pattern.search(ai_response)
+        if summary_match:
+            summary = summary_match.group(1).strip()
 
-    # Extract hashtags from AI response
-    hashtags_pattern = re.compile(r'Hashtag:\s*(.*?)$', re.DOTALL)
-    hashtags_match = hashtags_pattern.search(ai_response)
-    if hashtags_match:
-        ai_hashtags = [f"#{tag.strip()}" for tag in hashtags_match.group(1).strip().split('#') if tag.strip()]
+        # Extract hashtags from AI response
+        hashtags_pattern = re.compile(r'Hashtag:\s*(.*?)$', re.DOTALL)
+        hashtags_match = hashtags_pattern.search(ai_response)
+        if hashtags_match:
+            ai_hashtags = [f"#{tag.strip()}" for tag in hashtags_match.group(1).strip().split('#') if tag.strip()]
 
-    # Extract hashtags from the user message
-    user_hashtags = [f"#{tag.strip()}" for tag in re.findall(r"#(\w+)", user_message)]
+        # Extract message_output
+        cut_message = ai_response.split("Summary:")[0]
+        if " Actual Output:" in cut_message:
+            cut_message = cut_message.split(" Actual Output:")[-1].strip()
 
-    # Combine AI hashtags and user hashtags
-    combined_hashtags = list(set(ai_hashtags + user_hashtags))
+    if user_message:
+        # Extract hashtags from the user message
+        user_hashtags = [f"#{tag.strip()}" for tag in re.findall(r"#(\w+)", user_message)]
+        # Combine AI hashtags and user hashtags
+        combined_hashtags = list(set(ai_hashtags + user_hashtags))
+    else:
+        combined_hashtags = ai_hashtags
 
     # Convert combined hashtags to a comma-separated string
     combined_hashtags_str = ', '.join(combined_hashtags)
-
-    # Extract message_output
-    cut_message = ai_response.split("Summary:")[0]
-    if " Actual Output:" in cut_message:
-        cut_message = cut_message.split(" Actual Output:")[-1].strip()
 
     # Compute embeddings
     summary_embedding = get_vector_from_embedding(summary)
