00f688d9c0c987930106539939576192f52ece8d
vanbnh <bui_ngoc_huy_van@ca-adv.co.jp>
2024-08-12 17:05:35 +0700

    remove token on admin site

--- core_app/admin.py
+++ core_app/admin.py
@@ -1,7 +1,6 @@
 from django.contrib import admin
 from core_app.models import Conversation, SystemPrompt, ExternalKnowledge, Agent, AgentTool, InternalKnowledge
 # Register your models here.
-from rest_framework.authtoken.models import Token
 
 admin.site.register(Conversation)
 admin.site.register(SystemPrompt)
@@ -9,4 +8,3 @@ admin.site.register(InternalKnowledge)
 admin.site.register(ExternalKnowledge)
 admin.site.register(Agent)
 admin.site.register(AgentTool)
-admin.site.register(Token)
