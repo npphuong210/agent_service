b2554fbf0ae969c9abdc20ce4c72e1f53e47dcac
vanbnh <bui_ngoc_huy_van@ca-adv.co.jp>
2024-08-17 19:56:28 +0700

    fix bearer authenticate

--- core_app/authentication.py
+++ core_app/authentication.py
@@ -1,37 +1,72 @@
-from rest_framework.authentication import BaseAuthentication
-from rest_framework.exceptions import AuthenticationFailed
-from rest_framework import HTTP_HEADER_ENCODING
+from rest_framework.authentication import BaseAuthentication, get_authorization_header
+from rest_framework import HTTP_HEADER_ENCODING, exceptions
 from rest_framework.authtoken.models import Token
 from django.core.exceptions import ObjectDoesNotExist
 
+from django.utils.translation import gettext_lazy as _
+
+
 class BearerTokenAuthentication(BaseAuthentication):
+    """
+    Simple token based authentication.
+
+    Clients should authenticate by passing the token key in the "Authorization"
+    HTTP header, prepended with the string "Token ".  For example:
+
+        Authorization: Token 401f7ac837da42b97f613d789819ff93537bee6a
+    """
+
+    keyword = ['Token', 'Bearer']
+    model = None
+
+    def get_model(self):
+        if self.model is not None:
+            return self.model
+        from rest_framework.authtoken.models import Token
+        return Token
+
+    """
+    A custom token model may be used, but must have the following properties.
+
+    * key -- The string identifying the token (Bearer or Token)
+    * user -- The user to which the token belongs
+    """
+
     def authenticate(self, request):
-        auth = request.headers.get('Authorization', None)
-        if not auth:
+        auth = get_authorization_header(request).split()
+
+        if not auth or auth[0].lower() not in [kw.lower().encode() for kw in self.keyword]:
             return None
 
-        # Xử lý Bearer token
+        if len(auth) == 1:
+            msg = _('Invalid token header. No credentials provided.')
+            raise exceptions.AuthenticationFailed(msg)
+        elif len(auth) > 2:
+            msg = _('Invalid token header. Token string should not contain spaces.')
+            raise exceptions.AuthenticationFailed(msg)
+
         try:
-            prefix, token = auth.split(' ')
-            if prefix.lower() != 'bearer':
-                raise AuthenticationFailed('Invalid token prefix')
-        except ValueError:
-            raise AuthenticationFailed('Invalid token header format')
+            token = auth[1].decode()
+        except UnicodeError:
+            msg = _('Invalid token header. Token string should not contain invalid characters.')
+            raise exceptions.AuthenticationFailed(msg)
 
-        # Xác thực token
-        user = self.authenticate_token(token)
-        if user is None:
-            raise AuthenticationFailed('Invalid token')
+        return self.authenticate_credentials(token)
 
-        return (user, token)
+    def authenticate_credentials(self, key):
+        model = self.get_model()
+        try:
+            token = model.objects.select_related('user').get(key=key)
+        except model.DoesNotExist:
+            raise exceptions.AuthenticationFailed(_('Invalid token.'))
 
-    def authenticate_token(self, token):
-        # Thay đổi logic xác thực token của bạn ở đây
-        # Ví dụ đơn giản: kiểm tra token cụ thể
-        if token == 'VALID_TOKEN':
-            from django.contrib.auth.models import User
-            return User.objects.get(username='admin')  # Thay đổi theo logic của bạn
-        return None
+        if not token.user.is_active:
+            raise exceptions.AuthenticationFailed(_('User inactive or deleted.'))
+
+        return (token.user, token)
+
+    def authenticate_header(self, request):
+        return ",".join(self.keyword)
 
 def get_user_instance_by_token(request):
     try:
