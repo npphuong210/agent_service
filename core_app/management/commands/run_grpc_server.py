77bbd307e127212580181fba82a120227a0a4d33
= <thienkute01022002@gmail.com>
2024-10-07 13:19:35 +0700

    fix: grpc run command

--- core_app/management/commands/run_grpc_server.py
+++ core_app/management/commands/run_grpc_server.py
@@ -3,13 +3,21 @@ from core_app.grpc.grpc_server import serve
 import os
 import threading
 
+
 class Command(BaseCommand):
-    help = 'Starts the gRPC server and Django server'
+    help = 'Starts the gRPC server'
 
     def handle(self, *args, **options):
-        # Start gRPC server in a separate thread
-        grpc_thread = threading.Thread(target=serve)
-        grpc_thread.start()
+        serve()
 
-        # Start Django development server
-        os.system("python3 manage.py runserver")
+
+# class Command(BaseCommand):
+#     help = 'Starts the gRPC server and Django server'
+
+#     def handle(self, *args, **options):
+#         # Start gRPC server in a separate thread
+#         grpc_thread = threading.Thread(target=serve)
+#         grpc_thread.start()
+
+#         # Start Django development server
+#         #os.system("python3 manage.py runserver")
